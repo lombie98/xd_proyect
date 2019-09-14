78e0b8bc563a61438a6b243d29f4cada2eba8c6b
lombie98 <agustin.lombardo98@gmail.com>
2019-09-14 12:46:31 -0300

    xd boostrapeado

---
+++ EANT EJERCICIOS JS/validar-link.js
@@ -0,0 +1,30 @@
+links=document.querySelectorAll("nav > a") //indica que el elemento a tiene que ser hijo del nav ::: ejemplo generico padre > hijos
+
+links.forEach( funtion (link) { 
+	console.log("el argumento link contiene:")
+	console.log(link)
+	link.onclick = function (evento){
+		evento.preventDefault()
+		console.log(evento)
+		console.log(event.target.href)
+
+		var rta = confirm("estas seguros??")
+
+		if(rta){
+			console.log(' ahora deneria ir a ')
+			console.log(evento.target.href)
+			window.location.href = evento.target.href
+		}
+		else{
+			alert ("gracias por quedarte waching")
+		}
+	}
+})
+
+/*function clickEvent (link){
+	console.log("en el argumento link hay:")
+	console.log(link)
+}*/
+
+
+links.onclick (alert(message?: DOMString))
